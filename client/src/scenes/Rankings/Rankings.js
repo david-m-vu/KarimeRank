5b2b179279d9986df514e28acf2811247c3e59c3
William Trantan <wtrantan@gmail.com>
2024-06-20 13:57:37 -0700

    changed grid layout size

--- client/src/scenes/Rankings/Rankings.js
+++ client/src/scenes/Rankings/Rankings.js
@@ -67,18 +67,18 @@ const Rankings = (props) => {
                 <input className="bg-black md:w-1/5 text-white  bg-opacity-50 rounded-md text-center p-[0.5rem] text-[1.5rem]" onKeyDown={handleSearch} placeholder="Add Idol" value={idolNameInput} onChange={handleIdolNameInputChange} type="text" name="search"></input>
                 <div className="resultMsg absolute text-black">{resultMsg}</div>
             </div>
-            <div className="images flex flex-row flex-wrap md:gap-10 gap-6 md:p-8 justify-center p-4">
+            <div className="images flex flex-row gap-3 md:gap-6 flex-wrap md:p-8 justify-center ">
                 {images.map((image, index) => {
                     return (
-                        <div key={image._id} className="rounded-xl p-2 bg-white shadow-2xl">
-                            <div className="flex flex-row justify-center">
-                                <img className=" border-4 border-black md:h-[30rem] h-[20rem] rounded-xl" src={image.imageUrl} alt={image.imageName} />
+                        <div key={image._id} className=" rounded-xl p-1 bg-white shadow-2xl ">
+                            <div className="flex flex-row justify-center ">
+                                <img className=" border-4 border-black md:h-[15rem] h-[5rem] rounded-xl" src={image.imageUrl} alt={image.imageName} />
                             </div>
-                            <div className="flex flex-row items-center gap-8 flex-wrap">
-                                <div className=" md:text-[6rem] text-[4rem] rankNumber">{index + 1}.</div>
+                            <div className="flex flex-row items-center md:gap-4 flex-wrap">
+                                <div className=" md:text-[2.5rem] text-[1rem] rankNumber">{index + 1}.</div>
                                 <div className="flex flex-col justify-center flex-1 items-center">
-                                    <div className="text-center text-[2rem] md:text-[2rem]">W: {image.numWins} L: {image.numLosses}</div>
-                                    <div className="text-center text-[1rem] md:text-[2rem]">
+                                    <div className="text-center text-[0.5rem] md:text-[1.5rem]">W: {image.numWins} L: {image.numLosses}</div>
+                                    <div className="text-center text-[0.3rem] md:text-[1rem]">
                                         Score: {image.score}
                                     </div>
                                 </div>
