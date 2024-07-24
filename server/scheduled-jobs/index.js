0864f883f03c6f1c55f2f6de26904415e40314eb
David Vu <david.vu@berkeley.edu>
2024-07-24 10:51:49 -0700

    scheduled archive every month'

--- server/scheduled-jobs/index.js
+++ server/scheduled-jobs/index.js
@@ -8,52 +8,49 @@ import schedule from "node-schedule";
 // const job = schedule.scheduleJob("0 0 1 * *", () => {
 //     console.log("HI")
 // })
-const job = schedule.scheduleJob("0 * * * * *", async () => {
-    const newList = await deleteIdol("lisa");
-    console.log("here");
+const job = schedule.scheduleJob("0 0 1 * *", async () => {
+    await archiveImages();
 })
 
-const deleteIdol = async (idolName) => {
+const archiveImages = async () => {
     try {
         const model = (process.env.TEST_MODE === "TEST_MODE") ? TestImage : Image;
- 
-        const imagesDeleted = await model.deleteMany({ idolName: new RegExp(`^${idolName}$`, 'i') });
-        const newImageObjects = await model.find();
- 
-        console.log("SUCCESS DAVE")
+
+        const archivedImages = moveDocuments(model, ArchivedImage);
+        if (!archivedImages) {
+            console.log("move didn't work, error")
+        }
+
+        console.log("successfully archived!");
     } catch (err) {
-        console.log("what", err.message);
+        console.log(err.message);
     }
 }
 
-const archiveImages = async (req, res) => {
-    try {
-        const model = (process.env.TEST_MODE === "TEST_MODE") ? TestImage : Image;
- 
-        const archivedImages = moveDocuments(model, ArchivedImage);
-        if (!archivedImages) {
-            res.status(500).json({ message: "move didn't work"})
-        }
- 
- 
-        res.status(200).json({ archivedImages })
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
- }
- 
- 
- const moveDocuments = async (SourceCollection, DestinationCollection) => {
+
+const moveDocuments = async (SourceCollection, DestinationCollection) => {
     try {
         const images = await SourceCollection.find();
         await DestinationCollection.insertMany(images);
         await SourceCollection.deleteMany({});
- 
- 
+
         console.log("poggers");
         const archivedImages = await DestinationCollection.find();
         return archivedImages
     } catch (err) {
         return null;
     }
- }
+}
+
+// const deleteIdol = async (idolName) => {
+//     try {
+//         const model = (process.env.TEST_MODE === "TEST_MODE") ? TestImage : Image;
+
+//         const imagesDeleted = await model.deleteMany({ idolName: new RegExp(`^${idolName}$`, 'i') });
+//         const newImageObjects = await model.find();
+
+//         console.log("SUCCESS DAVE")
+//     } catch (err) {
+//         console.log("what", err.message);
+//     }
+// }
